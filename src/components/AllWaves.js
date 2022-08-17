b503744e5f9c473903c7923bd0321b7944d8092e
ginnygu <ginnygu@gmail.com>
2022-08-16 22:50:58 -0400

    update allwaves.js

--- src/components/AllWaves.js
+++ src/components/AllWaves.js
@@ -2,11 +2,12 @@ import React from "react";
 import "../App.css";
 
 export default function AllWaves({ wave }) {
+	const { message, address, timestamp } = wave;
 	return (
 		<div>
-			<div>Message: {wave.message}</div>
-			<div>Address: {wave.address}</div>
-			<div>Timestamp: {wave.timestamp.toString()}</div>
+			<div>Message: {message}</div>
+			<div>Address: {address}</div>
+			<div>Timestamp: {timestamp.toString()}</div>
 		</div>
 	);
 }
